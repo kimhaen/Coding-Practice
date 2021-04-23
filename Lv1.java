5e7ec657fe82db914f2deb023cb6c469be230ad8
Kimhaen <>
2021-04-23 06:53:06 +0000

    day2

--- Lv1.java
+++ Lv1.java
@@ -50,5 +50,23 @@ class Lv1 {
         return answer;
     }
   
+
+  public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for (int i=0; i<commands.length; i++) {
+            int[] result = new int[commands[i][1] - (commands[i][0]-1)];
+            
+            for (int j=0; j<result.length; j++) {
+                result[j] = array[j + commands[i][0]-1];
+            }
+            Arrays.sort(result);
+            answer[i] = result[commands[i][2]-1];
+        }
+        
+        return answer;
+    }
+
+
   
 }
--- Main.java
+++ Main.java
@@ -2,7 +2,10 @@ import java.util.*;
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    
+    //출력테스트
+
+    System.out.println("+++++++++ 출력테스트 ++++++++++");
     String rtn = "";
     int[] sort = {};
     Lv1 lv1 = new Lv1();
@@ -13,6 +16,18 @@ class Main {
 
     sort = lv1.solution();
     System.out.println(Arrays.toString(sort));
+
+
+    //K번째 수 Solution
+
+    System.out.println("+++++++++ K번째 수 Solution ++++++++++");
+    int[] array = {1, 5, 2, 6, 3, 7, 4};
+    int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+
+    int[] ans = {};
+
+    ans = lv1.solution(array,commands);
+    System.out.println(Arrays.toString(ans));
 
   }
 }
